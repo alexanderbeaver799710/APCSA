0b344a67525d3c72d98cbdabf94e60db871efb6b
Alexander Beaver <noxbin10136@outlook.com>
2018-10-21 12:58:44 -0700

    Finish Magpie activity 3

--- Projects/Project3 - MagPie/MagpieActivityStarterCode/activity3/StringExplorer.java
+++ Projects/Project3 - MagPie/MagpieActivityStarterCode/activity3/StringExplorer.java
@@ -7,21 +7,26 @@
 public class StringExplorer
 {
 
-	public static void main(String[] args)
-	{
-		String sample = "The quick brown fox jumped over the lazy dog.";
-		
-		//  Demonstrate the indexOf method.
-		int position = sample.indexOf("quick");
-		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
-		//  Demonstrate the toLowerCase method.
-		String lowerCase = sample.toLowerCase();
-		System.out.println ("sample.toLowerCase() = " + lowerCase);
-		System.out.println ("After toLowerCase(), sample = " + sample);
-		
-		//  Try other methods here:
-
-	}
+    public static void main(String[] args)
+    {
+        String sample = "The quick brown fox jumped over the lazy dog.";
+        
+        //  Demonstrate the indexOf method.
+        int position = sample.indexOf("quick");
+        System.out.println ("sample.indexOf(\"quick\") = " + position);
+        
+        //  Demonstrate the toLowerCase method.
+        String lowerCase = sample.toLowerCase();
+        System.out.println ("sample.toLowerCase() = " + lowerCase);
+        System.out.println ("After toLowerCase(), sample = " + sample);
+        
+        //  Try other methods here:
+        
+        int pos = 0;
+        while(pos > -1){
+            pos = sample.indexOf("e", pos + 1);
+            System.out.println(pos);
+        }
+    }
 
 }
