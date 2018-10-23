7b9bb05583bf03cf2126a58a357ed16de8a9638c
Alex Beaver <31376007+alexanderbeaver799710@users.noreply.github.com>
2018-10-23 09:29:24 -0700

    Finish Magpie Activity

--- Projects/Project3 - MagPie/MagpieActivityStarterCode/activity5/Magpie5.java
+++ Projects/Project3 - MagPie/MagpieActivityStarterCode/activity5/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"Interesting",
+			"Really?",
+			"Fascinating",
+			"Wow!"
 	};
 	
 }
